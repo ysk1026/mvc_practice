61f959d845d86e936432fc53815dc78d8f9429a2
Kim <fkqoseka@gmail.com>
2020-12-14 18:50:18 +0900

    cors

--- api/main.py
+++ api/main.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+
+from com_mini_api.ext.db import url, db
+from com_mini_api.ext.routes import initialize_routes
 
 app = Flask(__name__)
+CORS(app, resources={r'/api/*': {"origins": "*"}})
 
-@app.route('/')
-def index():
-    return ('Hello flask')
+app.config['SQLALCHEMY_DATABASE_URI'] = url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+api = Api(app)
 
-if __name__ == '__main__':
-    app.run()
+with app.app_context():
+    db.create_all()
+    
+print('********** INITIALIZE **********')
+initialize_routes(api)
