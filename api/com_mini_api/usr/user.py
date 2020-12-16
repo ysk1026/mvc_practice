b30b133823a64066a68077b209ee74ff46753155
Kim <fkqoseka@gmail.com>
2020-12-16 22:44:49 +0900

    nav

---
+++ api/com_mini_api/usr/user.py
@@ -0,0 +1,3 @@
+from sqlalchemy import func
+import pandas as pd
+import json
---
+++ ui/src/components/Nav.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Nav from 'react-router';
+
+const Nav = () => {
+    return {
+        <Nav></Nav>
+    }
+}
