cd0e8b4100625d7f2008f62fbb6698cf4cec2f1c
Kim <fkqoseka@gmail.com>
2020-12-25 21:56:41 +0900

    cs

--- ui/src/templates/User.jsx
+++ ui/src/templates/User.jsx
@@ -5,3 +5,24 @@ const User = () => {
 }
 
 export default User
+import React from 'react';
+import Link from 'react-router';
+
+const Nav = () => {
+    return(
+        <ol>
+            <li>
+                <Link>Home</Link>
+            </li>
+            <li>
+                <Link>Dance</Link>
+            </li>
+            <li>
+                <Link>Lesson</Link>
+            </li>
+            <li>
+                <Link>Contact</Link>
+            </li>
+        </ol>
+    )
+}
