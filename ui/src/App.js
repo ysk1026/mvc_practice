476a27ed990689a527d31a715d2917d95319f187
Kim <fkqoseka@gmail.com>
2020-12-26 19:30:46 +0900

    router

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import {Nav} from '../components'
-import {User} from '../templates'
+import {Nav} from '../components';
+import {User} from '../templates';
+import {BrowserRouter as Router, Route, Switch } from 'react-router';
 
 function App() {
-  return (
-    <User>Hello React</User>
-  );
+  return <>
+    {/* <User>Hello React</User> */}
+    <Nav></Nav>
+    <Router>
+      <Switch>
+        <Route path="/user" component ={User}/>
+      </Switch>
+    </Router>
+  </>
 }
 
 export default App;
